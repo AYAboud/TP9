47e0c976cec442d14339ef7bd23beb77adf4d58e
AYAboud <boudnaniaya@example.com>
2025-11-12 23:26:28 +0100

    B A A Initial commit
    A
    A

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 # TP9
+=======
+
+
+Uploading React App — Mozilla Firefox 2025-10-30 01-21-01.mp4…
+
+>>>>>>> ffc50ee (Initial commit)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CompteList from './components/CompteList';
+import CompteForm from './components/CompteForm';
+
+function App() {
+  return (
+    <div>
+      <CompteForm />
+      <CompteList />
+    </div>
+  );
+}
+
+export default App;
